ceaab5ff45518542015e2f8080862e705253353c
Quentin Perthuis <perthuis.quentin@gmail.com>
2026-03-06 12:32:39 +0100

    :recycle: Refacto file folders (#24)

--- commands.js
+++ commands.js
@@ -1,6 +1,6 @@
 import 'dotenv/config';
-import { InstallGlobalCommands } from './commons/utils.js';
-import { commands } from './commands/index.js';
+import { InstallGlobalCommands } from './app/commons/utils.js';
+import { commands } from './app/commands/index.js';
 
 const commandDefinitions = commands.map((cmd) => cmd.definition);
 
